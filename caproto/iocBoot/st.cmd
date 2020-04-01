2b92ac4e90c015a2c04197a295da761cd9e9b68a
rajanplumley <rajanplumley@gmail.com>
2020-03-31 22:59:01 -0700

    Add iocBoot for running caproto IOC

--- caproto/iocBoot/st.cmd
+++ caproto/iocBoot/st.cmd
@@ -6,7 +6,7 @@ TOP=../
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
 
-###############################
+##########################################
 # use Raj's env for development
 export PCDS_CONDA_VER='3.2.0'
 source /reg/g/pcds/pyps/conda/pcds_conda
@@ -14,7 +14,7 @@ source /reg/g/pcds/pyps/conda/pcds_conda
 PYDEV=/reg/neh/home/rajan-01/pydev
 export PATH="${PYDEV}/bin:${PATH}"
 export PYTHONPATH="${PYDEV}:${PYTHONPATH}"
-###############################
+##########################################
 
 cd "$(dirname "$0")"
 python --version
