b1714743471446d947cb1e9663318865d4d77473
K Lauer <klauer@slac.stanford.edu>
2020-07-27 14:17:44 -0700

    REF: load default table on init

--- ioc-satt-dev/db/filters.py
+++ ioc-satt-dev/db/filters.py
@@ -8,14 +8,20 @@ class FilterGroup(PVGroup):
     """
     PV group for filter metadata.
     """
+    def __init__(self, prefix, *, ioc, index, **kwargs):
+        super().__init__(prefix, **kwargs)
+        self.ioc = ioc
+        self.index = index
+        # Default to silicon, for now
+        self.load_data('Si')
 
-    async def load_data(self, instance, value):
+    def load_data(self, formula):
         """
         Load the HDF5 dataset containing physical constants
         and photon energy : atomic scattering factor table.
         """
-        self.log.info("Loading absorption table for %s...", value)
-        self.table = calculator.get_absorption_table(formula=value)
+        self.log.info("Loading absorption table for %s...", formula)
+        self.table = calculator.get_absorption_table(formula=formula)
         self.eV_min = self.table[0, 0]
         self.eV_max = self.table[-1, 0]
         self.eV_inc = (self.eV_max - self.eV_min) / len(self.table[:, 0])
@@ -26,20 +32,22 @@ class FilterGroup(PVGroup):
             'table': self.table
         }
         self.log.info("Absorption table successfully loaded.")
-        return value
 
     material = pvproperty(value='Si',
-                          put=load_data,
                           name='MATERIAL',
                           record='stringin',
                           doc='Filter material',
                           dtype=ChannelType.STRING)
 
+    @material.putter
+    async def material(self, instance, value):
+        self.load_data(formula=value)
+
     thickness = pvproperty(value=1E-6,
                            name='THICKNESS',
                            record='ao',
-                           upper_alarm_limit=1.0,
-                           lower_alarm_limit=0.0,
+                           upper_ctrl_limit=1.0,
+                           lower_ctrl_limit=0.0,
                            doc='Filter thickness',
                            units='m')
 
@@ -58,8 +66,8 @@ class FilterGroup(PVGroup):
 
     @pvproperty(name='T',
                 value=0.5,
-                upper_alarm_limit=1.0,
-                lower_alarm_limit=0.0,
+                upper_ctrl_limit=1.0,
+                lower_ctrl_limit=0.0,
                 read_only=True)
     async def transmission(self, instance):
         return self.get_transmission(
@@ -76,11 +84,6 @@ class FilterGroup(PVGroup):
             3 * self.current_photon_energy,
             self.thickness.value)
 
-    def __init__(self, prefix, *, ioc, index, **kwargs):
-        super().__init__(prefix, **kwargs)
-        self.ioc = ioc
-        self.index = index
-
     @property
     def current_photon_energy(self):
         """Current photon energy in eV."""
@@ -94,24 +97,6 @@ class FilterGroup(PVGroup):
 
     @thickness.putter
     async def thickness(self, instance, value):
-        if value < 0:
-            raise ValueError('Thickness must be a positive number')
         await self.transmission.write(
             self.get_transmission(self.current_photon_energy, value)
         )
-
-    @material.startup
-    async def material(self, instance, value):
-        await instance.write("Si")
-
-    @closest_eV.startup
-    async def closest_eV(self, instance, value):
-        closest_eV, _ = calculator.find_closest_energy(
-            self.current_photon_energy, self.table)
-        await instance.write(closest_eV)
-
-    @closest_eV_index.startup
-    async def closest_eV_index(self, instance, value):
-        _, closest_idx = calculator.find_closest_energy(
-            self.current_photon_energy, self.table)
-        await instance.write(closest_idx)
