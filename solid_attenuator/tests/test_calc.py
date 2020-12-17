e2092b0ea0760adb7c49e11071c7033dfa2b1605
K Lauer <klauer@slac.stanford.edu>
2020-12-17 11:31:13 -0800

    TST: brute-force verification of silicon filter protection

--- solid_attenuator/tests/test_calc.py
+++ solid_attenuator/tests/test_calc.py
@@ -1,90 +1,103 @@
 import dataclasses
 
+import numpy as np
 import pytest
 
-from ..ioc_lfe_at2l0_calc.calculator import \
-    get_best_config_with_material_priority
+from ..ioc_lfe_at2l0_calc import calculator
 
 
 @dataclasses.dataclass
 class Filter:
-    idx: int
     material: str
+    thickness: float
     transmission: float
 
 
-filters = [
-    Filter(2, 'C', 0.3134962503250302),
-    Filter(4, 'C', 0.5598988143521115),
-    Filter(3, 'C', 0.7482610191670119),
-    Filter(5, 'C', 0.8650199981094857),
-    Filter(6, 'C', 0.9300642920693629),
-    Filter(7, 'C', 0.9643983521870266),
-    Filter(8, 'C', 0.9820378422346253),
-    Filter(9, 'C', 0.9909782212303363),
-    Filter(10, 'Si', 3.1625271172427536e-39),
-    Filter(11, 'Si', 5.117269395605226e-20),
-    Filter(12, 'Si', 2.2106031589871655e-10),
-    Filter(13, 'Si', 1.478712376111808e-05),
-    Filter(14, 'Si', 0.0038403455280337836),
-    Filter(15, 'Si', 0.061950552529553324),
-    Filter(16, 'Si', 0.24887884299621377),
-    Filter(17, 'Si', 0.4988676984793939),
-    Filter(18, 'Si', 0.7063021804584021),
-    Filter(19, 'Si', 0.8404168242711676)
-]
+@pytest.fixture(params=range(2_000, 3_0000, 2_000))
+def photon_energy(request):
+    return float(request.param)
 
 
-test_cases = [
-    (0.0, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
-    (0.00344, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1]),
-    (0.00689, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]),
-    (0.01034, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1]),
-    (0.01379, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1]),
-    (0.01724, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0]),
-    (0.02068, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1]),
-    (0.02413, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]),
-    (0.02758, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]),
-    (0.03103, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0]),
-    (0.03448, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0]),
-    (0.03793, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1]),
-    (0.04137, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1]),
-    (0.04482, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]),
-    (0.04827, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]),
-    (0.05172, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]),
-    (0.05517, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]),
-    (0.05862, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]),
-    (0.06206, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]),
-    (0.06551, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]),
-    (0.06896, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]),
-    (0.07241, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
-    (0.07586, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
-    (0.07931, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
-    (0.08275, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
-    (0.08620, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.08965, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.09310, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.09655, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.1, [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.1, [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.2, [1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.3, [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.4, [0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.5, [0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.6, [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.7, [0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.8, [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (0.9, [0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    (1.0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-]
+absorption_tables = {}
 
 
-@pytest.mark.parametrize('t_des, states', test_cases)
-def test_material_prioritization(t_des, states):
-    conf = get_best_config_with_material_priority(
-        materials=[flt.material for flt in filters],
-        transmissions=[flt.transmission for flt in filters],
-        material_order=['C', 'Si'],
-        t_des=t_des,
+def get_transmission(material: str,
+                     thickness: float,
+                     photon_energy: float) -> np.ndarray:
+    """Get the transmission for a given material at a specific energy."""
+    try:
+        table = absorption_tables[material]
+    except KeyError:
+        table = calculator.get_absorption_table(material)
+        absorption_tables[material] = table
+    return calculator.get_transmission(
+        photon_energy, table=table, thickness=thickness * 1e-6
     )
-    assert (conf.filter_states == states).all()
+
+
+@pytest.mark.parametrize(
+    'diamond_thicknesses, si_thicknesses',
+    [
+        pytest.param(
+            [1280, 640, 320, 160, 80, 40, 20, 10],
+            [10240, 5120, 2560, 1280, 640, 320, 160, 80, 40, 20],
+            id='all_working'
+        ),
+        pytest.param(
+            [640, 320, 160, 80, 40, 20, 10],
+            [10240, 5120, 2560, 1280, 640, 320, 160, 80, 40, 20],
+            id='missing_C_1280'
+        ),
+        pytest.param(
+            [640, 320, 160, 80, 40],
+            [10240, 5120, 2560, 1280, 640, 320, 160, 80, 40, 20],
+            id='missing_C_10_20'
+        ),
+    ]
+)
+def test_material_prioritization(diamond_thicknesses, si_thicknesses,
+                                 photon_energy):
+    diamond_filters = [
+        Filter('C', thickness, get_transmission('C', thickness, photon_energy))
+        for thickness in diamond_thicknesses
+    ]
+    silicon_filters = [
+        Filter('Si', thickness, get_transmission('Si', thickness,
+                                                 photon_energy))
+        for thickness in si_thicknesses
+    ]
+    filters = diamond_filters + silicon_filters
+
+    t_all_diamond = np.product([flt.transmission for flt in diamond_filters])
+
+    t_des_checks = (
+        list(np.linspace(0.0, t_all_diamond, 1500)) +
+        list(np.linspace(t_all_diamond, 1.0, 1500))
+    )
+
+    materials = [flt.material for flt in filters]
+    transmissions = [flt.transmission for flt in filters]
+
+    compared = []
+    for t_des in t_des_checks:
+        conf = calculator.get_best_config_with_material_priority(
+            materials=materials,
+            transmissions=transmissions,
+            material_order=['C', 'Si'],
+            t_des=t_des,
+        )
+
+        inserted_materials = [
+            flt.material for state, flt in zip(conf.filter_states, filters)
+            if state
+        ]
+        if 'Si' in inserted_materials:
+            assert inserted_materials.count('C') == len(diamond_filters)
+
+        if t_des > 0.0:
+            compared.append(abs(t_des - conf.transmission) / t_des)
+
+    print()
+    print('(t_des - t_act) / t_des:')
+    print('    Standard deviation:', np.std(compared))
+    print('    Average:', np.average(compared))
