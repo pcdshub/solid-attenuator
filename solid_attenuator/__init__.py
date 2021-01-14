06099d98b2cde567c55ebcb63dcf4f714bd9dcdb
K Lauer <klauer@slac.stanford.edu>
2021-01-14 13:08:23 -0800

    MNT: fix up top-level __all__

--- solid_attenuator/__init__.py
+++ solid_attenuator/__init__.py
@@ -1,7 +1,18 @@
+from . import calculator, filters, ioc, sxr, system, util
 from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
 
-# TODO: fill this in with appropriate star imports:
-__all__ = []
+__all__ = [
+    'calculator',
+    'filters',
+    'ioc',
+    'sxr',
+    'system',
+    'util',
+    'ioc_kfe_at1k4_calc',
+    'ioc_lfe_at2l0_calc',
+    'ioc_sim_at2l0',
+    'ioc_sim_sxr',
+]
