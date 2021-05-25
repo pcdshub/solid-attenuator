9d84f4da9237c3ac18ebc9af05e6be0b6dc896ae
Ken Lauer <klauer@slac.stanford.edu>
2021-05-25 13:04:44 -0700

    TST: clean up tests; generate more plots

--- solid_attenuator/tests/test_calc.py
+++ solid_attenuator/tests/test_calc.py
@@ -8,7 +8,7 @@ from .. import calculator
 
 import matplotlib  # isort: skip  # noqa
 
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 
 import matplotlib.pyplot as plt  # isort: skip  # noqa
 
@@ -25,7 +25,7 @@ class Blade:
     filters: List[Filter]
 
 
-@pytest.fixture(params=[2000, 3000, 5000, 8000, 12_000, 20_000, 25_000])
+@pytest.fixture(params=[1000, 6000, 9000, 12_000, 20_000, 25_000])
 def photon_energy(request) -> float:
     return float(request.param)
 
@@ -35,8 +35,7 @@ def soft_photon_energy(request) -> float:
     return float(request.param)
 
 
-@pytest.fixture(params=[calculator.ConfigMode.Floor,
-                        calculator.ConfigMode.Ceiling])
+@pytest.fixture(params=[calculator.ConfigMode.Floor, calculator.ConfigMode.Ceiling])
 def mode(request) -> calculator.ConfigMode:
     return request.param
 
@@ -45,9 +44,9 @@ absorption_tables = {}
 _subplots = {}
 
 
-def get_transmission(material: str,
-                     thickness: float,
-                     photon_energy: float) -> np.ndarray:
+def get_transmission(
+    material: str, thickness: float, photon_energy: float
+) -> np.ndarray:
     """Get the transmission for a given material at a specific energy."""
     try:
         table = absorption_tables[material]
@@ -59,45 +58,92 @@ def get_transmission(material: str,
     )
 
 
+def test_transmission_values():
+    photon_energy = np.arange(1000, 25000, 50)
+    for material in ["C", "Si"]:
+        plt.figure(figsize=(8, 6), dpi=200)
+        thicknesses = {
+            "C": [1280, 640, 320, 160, 80, 40, 20, 10],
+            "Si": [10240, 5120, 2560, 1280, 640, 320, 160, 80, 40, 20],
+        }[material]
+        for idx, thickness_um in enumerate(thicknesses):
+            c_transm = np.asarray(
+                [
+                    get_transmission(material, thickness_um, energy)
+                    for energy in photon_energy
+                ]
+            )
+
+            (line,) = plt.plot(
+                photon_energy,
+                c_transm,
+                label=f"{thickness_um} µm {material}",
+                alpha=0.5,
+                lw=1,
+            )
+            try:
+                about_half_energy = photon_energy[np.where(c_transm >= 0.5)[0][0]]
+            except IndexError:
+                ...
+            else:
+                plt.annotate(
+                    f"{thickness_um}µm",
+                    xy=(about_half_energy - 1000, 0.4 + 0.1 * (idx % 3)),
+                    color=line.get_color(),
+                )
+        # plt.legend(loc="best")
+        plt.suptitle("Transmission vs Photon Energy")
+        plt.title(f"{material} using CXRO atomic scattering factors")
+        plt.xlabel("Photon Energy [eV]")
+        plt.ylabel("Transmission")
+        plt.savefig(f"transmission_comparison_{material}.pdf")
+        plt.savefig(f"transmission_comparison_{material}.png")
+        plt.yscale("log")
+        plt.ylim(1e-5, 1.0)
+        plt.savefig(f"transmission_comparison_{material}_log.pdf")
+        plt.savefig(f"transmission_comparison_{material}_log.png")
+        plt.xlim(1000, 5000)
+        plt.savefig(f"transmission_comparison_{material}_log_5kev.pdf")
+        plt.savefig(f"transmission_comparison_{material}_log_5kev.png")
+
+
 @pytest.mark.parametrize(
-    'diamond_thicknesses, si_thicknesses',
+    "diamond_thicknesses, si_thicknesses",
     [
         pytest.param(
             [1280, 640, 320, 160, 80, 40, 20, 10],
             [10240, 5120, 2560, 1280, 640, 320, 160, 80, 40, 20],
-            id='all_working'
+            id="all_working",
         ),
         pytest.param(
             [640, 320, 160, 80, 40, 20, 10],
             [10240, 5120, 2560, 1280, 640, 320, 160, 80, 40, 20],
-            id='missing_C_1280'
+            id="missing_C_1280",
         ),
         pytest.param(
             [640, 320, 160, 80, 40],
             [10240, 5120, 2560, 1280, 640, 320, 160, 80, 40, 20],
-            id='missing_C_10_20'
+            id="missing_C_10_20",
         ),
-    ]
+    ],
 )
-def test_material_prioritization(request, diamond_thicknesses, si_thicknesses,
-                                 mode,
-                                 photon_energy):
+def test_material_prioritization(
+    request, diamond_thicknesses, si_thicknesses, mode, photon_energy
+):
     diamond_filters = [
-        Filter('C', thickness, get_transmission('C', thickness, photon_energy))
+        Filter("C", thickness, get_transmission("C", thickness, photon_energy))
         for thickness in diamond_thicknesses
     ]
     silicon_filters = [
-        Filter('Si', thickness, get_transmission('Si', thickness,
-                                                 photon_energy))
+        Filter("Si", thickness, get_transmission("Si", thickness, photon_energy))
         for thickness in si_thicknesses
     ]
     filters = diamond_filters + silicon_filters
 
     t_all_diamond = np.product([flt.transmission for flt in diamond_filters])
 
-    t_des_checks = (
-        list(np.linspace(0.0, t_all_diamond, 1500)) +
-        list(np.linspace(t_all_diamond, 1.0, 1500))
+    t_des_checks = list(np.linspace(0.0, t_all_diamond, 1500)) + list(
+        np.linspace(t_all_diamond, 1.0, 1500)
     )
 
     materials = [flt.material for flt in filters]
@@ -109,17 +155,16 @@ def test_material_prioritization(request, diamond_thicknesses, si_thicknesses,
         conf = calculator.get_best_config_with_material_priority(
             materials=materials,
             transmissions=transmissions,
-            material_order=['C', 'Si'],
+            material_order=["C", "Si"],
             t_des=t_des,
             mode=mode,
         )
 
         inserted_materials = [
-            flt.material for state, flt in zip(conf.filter_states, filters)
-            if state
+            flt.material for state, flt in zip(conf.filter_states, filters) if state
         ]
-        if 'Si' in inserted_materials:
-            assert inserted_materials.count('C') == len(diamond_filters)
+        if "Si" in inserted_materials:
+            assert inserted_materials.count("C") == len(diamond_filters)
 
         actual.append(conf.transmission)
         if t_des > 0.0:
@@ -128,14 +173,14 @@ def test_material_prioritization(request, diamond_thicknesses, si_thicknesses,
             compared.append(0.0)
 
     print()
-    print('(t_des - t_act) / t_des:')
-    print('    Standard deviation:', np.std(compared))
-    print('    Average:', np.average(compared))
+    print("(t_des - t_act) / t_des:")
+    print("    Standard deviation:", np.std(compared))
+    print("    Average:", np.average(compared))
 
     # I'm sure there's a better way to do this:
-    param_id = request.node.name.rsplit('-', 1)[1].strip(']')
+    param_id = request.node.name.rsplit("-", 1)[1].strip("]")
     # -> param_id = 'all_working'
-    param_id = f'{param_id}_{mode.name}'
+    param_id = f"{param_id}_{mode.name}"
 
     try:
         subplot = _subplots[param_id]
@@ -144,78 +189,103 @@ def test_material_prioritization(request, diamond_thicknesses, si_thicknesses,
         _subplots[param_id] = subplot = fig.subplots(1, 2)
 
         # fig.suptitle(param_id)
-        subplot[0].set_title('Transmission Actual vs Desired')
-        subplot[1].set_title('Error vs Desired')
+        subplot[0].set_title("Transmission Actual vs Desired")
+        subplot[1].set_title("Error vs Desired")
         # fig.tight_layout()
 
-    line, = subplot[0].plot(t_des_checks, actual,
-                            alpha=0.5,
-                            lw=1,
-                            label=f'{photon_energy} eV')
-    subplot[0].plot([t_all_diamond], [t_all_diamond],
-                    marker='x',
-                    markersize=3,
-                    markerfacecolor=line.get_color(),
-                    color=line.get_color(),
-                    )
+    (line,) = subplot[0].plot(
+        t_des_checks, actual, alpha=0.5, lw=1, label=f"{photon_energy} eV"
+    )
+    subplot[0].plot(
+        [t_all_diamond],
+        [t_all_diamond],
+        marker="x",
+        markersize=3,
+        markerfacecolor=line.get_color(),
+        color=line.get_color(),
+    )
     subplot[0].annotate(
-        f'{photon_energy:.0f} eV',
+        f"{photon_energy:.0f} eV",
         (t_all_diamond, t_all_diamond),
         (t_all_diamond - 0.2, t_all_diamond),
         color=line.get_color(),
     )
-    subplot[0].legend(loc='best')
+    subplot[0].legend(loc="best")
 
-    subplot[1].plot(t_des_checks,
-                    np.asarray(actual) - np.asarray(t_des_checks),
-                    label=f'{photon_energy} eV',
-                    alpha=0.5, lw=1, color=line.get_color())
+    subplot[1].plot(
+        t_des_checks,
+        np.asarray(actual) - np.asarray(t_des_checks),
+        label=f"{photon_energy} eV",
+        alpha=0.5,
+        lw=1,
+        color=line.get_color(),
+    )
     subplot[0].set_xlabel("Desired transmission")
     subplot[0].set_ylabel("Actual transmission")
+    # subplot[0].set_xscale("log")
+    # subplot[0].set_yscale("log")
+
     subplot[1].set_xlabel("Desired transmission")
     subplot[1].set_ylabel("(Actual - desired) transmission")
-    subplot[0].figure.savefig(f'{param_id}.pdf')
-    subplot[0].figure.savefig(f'{param_id}.png', transparent=True,
-                              bbox_inches='tight', pad_inches=0.2)
+
+    for idx, fn in enumerate((param_id, f"{param_id}_low_transm")):
+        if idx == 0:
+            subplot[0].set_xlim(0, 1.0)
+            subplot[0].set_ylim(0, 1.0)
+        else:
+            subplot[0].set_xlim(0, 0.1)
+            subplot[0].set_ylim(0, 0.1)
+        subplot[0].figure.savefig(f"{fn}.pdf")
+        subplot[0].figure.savefig(
+            f"{fn}.png", transparent=True, bbox_inches="tight", pad_inches=0.2
+        )
 
 
 @pytest.mark.parametrize(
-    'blades',
+    "blades",
     [
         pytest.param(
             [
-                Blade([
-                    Filter('C', 25),
-                    Filter('C', 50),
-                    Filter('C', 100),
-                    Filter('Si', 320),
-                    Filter('Si', 160),
-                    Filter('Si', 80),
-                    Filter('Si', 40),
-                    Filter('Si', 20),
-                ]),
-                Blade([
-                    Filter('C', 50),
-                    Filter('C', 25),
-                    Filter('C', 12),
-                    Filter('C', 10),
-                ]),
-                Blade([
-                    Filter('C', 25),
-                    Filter('C', 12),
-                    Filter('C', 6),
-                ]),
-                Blade([
-                    Filter('C', 12),
-                    Filter('C', 6),
-                    Filter('C', 3),
-                    Filter('C', 3),  # according to traveler
-                    Filter('Al', 0.2),
-                ]),
+                Blade(
+                    [
+                        Filter("C", 25),
+                        Filter("C", 50),
+                        Filter("C", 100),
+                        Filter("Si", 320),
+                        Filter("Si", 160),
+                        Filter("Si", 80),
+                        Filter("Si", 40),
+                        Filter("Si", 20),
+                    ]
+                ),
+                Blade(
+                    [
+                        Filter("C", 50),
+                        Filter("C", 25),
+                        Filter("C", 12),
+                        Filter("C", 10),
+                    ]
+                ),
+                Blade(
+                    [
+                        Filter("C", 25),
+                        Filter("C", 12),
+                        Filter("C", 6),
+                    ]
+                ),
+                Blade(
+                    [
+                        Filter("C", 12),
+                        Filter("C", 6),
+                        Filter("C", 3),
+                        Filter("C", 3),  # according to traveler
+                        Filter("Al", 0.2),
+                    ]
+                ),
             ],
-            id='at1k4'
+            id="at1k4",
         ),
-    ]
+    ],
 )
 def test_ladder(request, blades, mode, soft_photon_energy):
     photon_energy = soft_photon_energy
@@ -224,8 +294,7 @@ def test_ladder(request, blades, mode, soft_photon_energy):
     for blade in blades:
         for flt in blade.filters:
             flt.transmission = get_transmission(
-                flt.material, flt.thickness,
-                soft_photon_energy
+                flt.material, flt.thickness, soft_photon_energy
             )
 
     t_des_checks = np.linspace(0.0, 1.0, 2000)
@@ -247,14 +316,14 @@ def test_ladder(request, blades, mode, soft_photon_energy):
             compared.append(0.0)
 
     print()
-    print('(t_des - t_act) / t_des:')
-    print('    Standard deviation:', np.std(compared))
-    print('    Average:', np.average(compared))
+    print("(t_des - t_act) / t_des:")
+    print("    Standard deviation:", np.std(compared))
+    print("    Average:", np.average(compared))
 
     # I'm sure there's a better way to do this:
-    param_id = request.node.name.rsplit('-', 1)[1].strip(']')
+    param_id = request.node.name.rsplit("-", 1)[1].strip("]")
     # -> param_id = 'all_working'
-    param_id = f'{param_id}_{mode.name}'
+    param_id = f"{param_id}_{mode.name}"
 
     try:
         subplot = _subplots[param_id]
@@ -263,24 +332,28 @@ def test_ladder(request, blades, mode, soft_photon_energy):
         _subplots[param_id] = subplot = fig.subplots(1, 2)
 
         # fig.suptitle(param_id)
-        subplot[0].set_title('Transmission Actual vs Desired')
-        subplot[1].set_title('Error vs Desired')
+        subplot[0].set_title("Transmission Actual vs Desired")
+        subplot[1].set_title("Error vs Desired")
         # fig.tight_layout()
 
-    line, = subplot[0].plot(t_des_checks, actual,
-                            alpha=0.5,
-                            lw=1,
-                            label=f'{photon_energy} eV')
-    subplot[0].legend(loc='best')
+    (line,) = subplot[0].plot(
+        t_des_checks, actual, alpha=0.5, lw=1, label=f"{photon_energy} eV"
+    )
+    subplot[0].legend(loc="best")
 
-    subplot[1].plot(t_des_checks,
-                    np.asarray(actual) - np.asarray(t_des_checks),
-                    label=f'{photon_energy} eV',
-                    alpha=0.5, lw=1, color=line.get_color())
+    subplot[1].plot(
+        t_des_checks,
+        np.asarray(actual) - np.asarray(t_des_checks),
+        label=f"{photon_energy} eV",
+        alpha=0.5,
+        lw=1,
+        color=line.get_color(),
+    )
     subplot[0].set_xlabel("Desired transmission")
     subplot[0].set_ylabel("Actual transmission")
     subplot[1].set_xlabel("Desired transmission")
     subplot[1].set_ylabel("(Actual - desired) transmission")
-    subplot[0].figure.savefig(f'{param_id}.pdf')
-    subplot[0].figure.savefig(f'{param_id}.png', transparent=True,
-                              bbox_inches='tight', pad_inches=0.2)
+    subplot[0].figure.savefig(f"{param_id}.pdf")
+    subplot[0].figure.savefig(
+        f"{param_id}.png", transparent=True, bbox_inches="tight", pad_inches=0.2
+    )
